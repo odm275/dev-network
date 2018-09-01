109692f286ec5c182f9fae21b106a3fd588f86f0
odm275 <pomejia@gmail.com>
2018-08-31 21:46:08 -0500

    updated reference

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <footer className="bg-dark text-white mt-5 p-4 text-center">
-      Coded by <a href="https://odm275.github.io/portfolio/">Oscar Mejia</a>
+      Coded by <a href="https://www.oscarmejia.me/">Oscar Mejia</a>
     </footer>
   );
 };
