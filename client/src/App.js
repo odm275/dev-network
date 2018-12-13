cf02219780c82b403970bc3105767137c1740600
odm275 <pomejia@gmail.com>
2018-12-13 07:19:51 -0600

    fixed store bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,54 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { clearCurrentProfile } from "./actions/profileActions";
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
+import { setCurrentUser, logoutUser } from './actions/authActions'
+import { clearCurrentProfile } from './actions/profileActions'
 
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux'
+import store from './store'
 
-import PrivateRoutes from "./components/common/PrivateRoutes";
+import PrivateRoutes from './components/common/PrivateRoutes'
 
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import AddExperience from "./components/add-credentials/AddExperience";
-import AddEducation from "./components/add-credentials/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import NotFound from "./components/not-found/not-found";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import Dashboard from './components/dashboard/Dashboard'
+import CreateProfile from './components/create-profile/CreateProfile'
+import EditProfile from './components/edit-profile/EditProfile'
+import AddExperience from './components/add-credentials/AddExperience'
+import AddEducation from './components/add-credentials/AddEducation'
+import Profiles from './components/profiles/Profiles'
+import Profile from './components/profile/Profile'
+import NotFound from './components/not-found/not-found'
+import Posts from './components/posts/Posts'
+import Post from './components/post/Post'
 
-import "./App.css";
+import './App.css'
 
 //Same thing we did via our action in login, but want it for every page request.
 //Need to make sure user is actually logged in. No holes.
 // Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
-  setAuthToken(localStorage.jwtToken);
+  setAuthToken(localStorage.jwtToken)
   // Decode token and get user info and exp
-  const decoded = jwt_decode(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthenticated
-  store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded))
 
   // Check for expired token
-  const currentTime = Date.now() / 1000;
+  const currentTime = Date.now() / 1000
   if (decoded.exp < currentTime) {
     // Logout user
-    store.dispatch(logoutUser());
+    store.dispatch(logoutUser())
     // Clear current Profile
-    store.dispatch(clearCurrentProfile());
+    store.dispatch(clearCurrentProfile())
     // Redirect to login
-    window.location.href = "/login";
+    window.location.href = '/login'
   }
 }
 class App extends Component {
@@ -110,8 +110,8 @@ class App extends Component {
           </div>
         </Router>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
